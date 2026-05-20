d5f0f34d66568d1f65c750b8debbf27be8fef5e8
Saransh <saransh1337@gmail.com>
2026-05-20 22:05:14 +0530

    Refine MainWindowTheme with updated design tokens

--- FocusCue/MainWindowTheme.swift
+++ FocusCue/MainWindowTheme.swift
@@ -7,67 +7,104 @@ import AppKit
 import SwiftUI
 
 enum FCColorToken: CaseIterable {
+    case canvasBlack
+    case surfaceSlate
+    case surfaceRaised
+    case surfaceInset
+    case frameGray
+    case hazardWhite
+    case absoluteBlack
+    case textMuted
+    case textInverted
+    case cueMint
+    case cueMintBorder
+    case resyncViolet
+    case violetRule
+    case hoverBlue
+    case focusCyan
+    case readYellow
+    case recordingPink
+    case teleprompterOrange
+    case disabledGray
     case bgCanvas
     case bgCanvasTop
     case bgCanvasBottom
-    case surfaceGlass
-    case surfaceGlassStrong
-    case surfaceOverlay
-    case borderSubtle
     case borderFocus
     case textPrimary
     case textSecondary
     case textTertiary
     case accentPrimary
-    case accentInfo
-    case accentCTA
     case stateSuccess
     case stateWarning
     case stateError
 
     func color(in scheme: ColorScheme) -> Color {
-        switch (self, scheme) {
-        case (.bgCanvas, .dark): return .fcHex(0x090B10)
-        case (.bgCanvas, _): return .fcHex(0xF5F7FB)
-        case (.bgCanvasTop, .dark): return .fcHex(0x131824)
-        case (.bgCanvasTop, _): return .fcHex(0xFFFFFF)
-        case (.bgCanvasBottom, .dark): return .fcHex(0x090B10)
-        case (.bgCanvasBottom, _): return .fcHex(0xEAF0F9)
-        case (.surfaceGlass, .dark): return .fcRGBA(21, 27, 39, 0.78)
-        case (.surfaceGlass, _): return .fcRGBA(255, 255, 255, 0.74)
-        case (.surfaceGlassStrong, .dark): return .fcRGBA(27, 34, 48, 0.90)
-        case (.surfaceGlassStrong, _): return .fcRGBA(255, 255, 255, 0.90)
-        case (.surfaceOverlay, .dark): return .fcRGBA(14, 18, 26, 0.86)
-        case (.surfaceOverlay, _): return .fcRGBA(245, 251, 255, 0.82)
-        case (.borderSubtle, .dark): return .fcRGBA(186, 201, 227, 0.22)
-        case (.borderSubtle, _): return .fcRGBA(152, 170, 199, 0.34)
-        case (.borderFocus, .dark): return .fcRGBA(126, 182, 255, 0.56)
-        case (.borderFocus, _): return .fcRGBA(61, 142, 255, 0.62)
-        case (.textPrimary, .dark): return .fcHex(0xF2F6FF)
-        case (.textPrimary, _): return .fcHex(0x0E1525)
-        case (.textSecondary, .dark): return .fcHex(0xB2C1D9)
-        case (.textSecondary, _): return .fcHex(0x475774)
-        case (.textTertiary, .dark): return .fcHex(0x8A98AF)
-        case (.textTertiary, _): return .fcHex(0x6C7B95)
-        case (.accentPrimary, .dark): return .fcHex(0x35D0C1)
-        case (.accentPrimary, _): return .fcHex(0x1DB6A8)
-        case (.accentInfo, .dark): return .fcHex(0x71A9FF)
-        case (.accentInfo, _): return .fcHex(0x3C8DFF)
-        case (.accentCTA, .dark): return .fcHex(0xFFB36D)
-        case (.accentCTA, _): return .fcHex(0xF59A43)
-        case (.stateSuccess, .dark): return .fcHex(0x34D399)
-        case (.stateSuccess, _): return .fcHex(0x1FA972)
-        case (.stateWarning, .dark): return .fcHex(0xF4B45F)
-        case (.stateWarning, _): return .fcHex(0xD98A2A)
-        case (.stateError, .dark): return .fcHex(0xFF7B87)
-        case (.stateError, _): return .fcHex(0xD4545D)
-        @unknown default:
-            return .accentColor
+        switch self {
+        case .canvasBlack, .bgCanvas, .bgCanvasTop, .bgCanvasBottom:
+            return .fcHex(0x131313)
+        case .surfaceSlate:
+            return .fcHex(0x2D2D2D)
+        case .surfaceRaised:
+            return .fcHex(0x1B1B1B)
+        case .surfaceInset:
+            return .fcHex(0x0F0F0F)
+        case .frameGray:
+            return .fcHex(0x313131)
+        case .hazardWhite, .textPrimary:
+            return .fcHex(0xFFFFFF)
+        case .absoluteBlack:
+            return .fcHex(0x000000)
+        case .textSecondary:
+            return .fcHex(0x949494)
+        case .textMuted:
+            return .fcHex(0xE9E9E9)
+        case .textTertiary:
+            return .fcHex(0x949494).opacity(0.72)
+        case .textInverted:
+            return .fcHex(0x131313)
+        case .cueMint, .accentPrimary:
+            return .fcHex(0x3CFFD0)
+        case .cueMintBorder:
+            return .fcHex(0x309875)
+        case .resyncViolet, .stateError:
+            return .fcHex(0x5200FF)
+        case .violetRule:
+            return .fcHex(0x3D00BF)
+        case .hoverBlue:
+            return .fcHex(0x3860BE)
+        case .focusCyan, .borderFocus:
+            return .fcHex(0x1EAEDB)
+        case .readYellow:
+            return .fcHex(0xFFD60A)
+        case .recordingPink:
+            return .fcHex(0xFF6191)
+        case .teleprompterOrange:
+            return .fcHex(0xFF9E0A)
+        case .stateSuccess:
+            return .fcHex(0x22C55E)
+        case .stateWarning:
+            return .fcHex(0xFACC15)
+        case .disabledGray:
+            return .fcHex(0x5A5A5A)
         }
     }
 }
 
 enum FCTypographyToken: CaseIterable {
+    case displayHero
+    case displayCompact
+    case titleLg
+    case titleMd
+    case titleSm
+    case body
+    case bodyCompact
+    case scriptLg
+    case scriptXl
+    case scriptDisplay
+    case labelCaps
+    case monoTimestamp
+    case monoButton
+    case counter
     case display
     case titleL
     case titleM
@@ -80,53 +117,96 @@ enum FCTypographyToken: CaseIterable {
 
     var size: CGFloat {
         switch self {
-        case .display: return 34
-        case .titleL: return 28
-        case .titleM: return 22
-        case .heading: return 18
-        case .bodyL: return 15
-        case .bodyM: return 14
-        case .label: return 13
-        case .caption: return 12
-        case .mono: return 12
+        case .displayHero, .scriptDisplay: return 72
+        case .displayCompact, .display: return 60
+        case .titleLg, .titleL: return 34
+        case .titleMd, .titleM: return 24
+        case .titleSm, .heading: return 20
+        case .body, .bodyL: return 16
+        case .bodyCompact, .bodyM, .label: return 13
+        case .scriptLg: return 24
+        case .scriptXl: return 48
+        case .labelCaps, .mono, .monoButton, .caption: return 12
+        case .monoTimestamp: return 11
+        case .counter: return 10
         }
     }
 
     var lineHeight: CGFloat {
         switch self {
-        case .display: return 40
-        case .titleL: return 34
-        case .titleM: return 28
-        case .heading: return 24
-        case .bodyL: return 22
-        case .bodyM: return 20
-        case .label: return 18
-        case .caption: return 16
-        case .mono: return 16
+        case .displayHero: return 65
+        case .displayCompact, .display: return 57
+        case .titleLg, .titleL: return 34
+        case .titleMd, .titleM: return 25
+        case .titleSm, .heading: return 22
+        case .body, .bodyL: return 26
+        case .bodyCompact, .bodyM, .label: return 19
+        case .scriptLg: return 32
+        case .scriptXl: return 65
+        case .scriptDisplay: return 97
+        case .labelCaps, .monoButton: return 14
+        case .monoTimestamp: return 13
+        case .counter: return 14
+        case .caption, .mono: return 16
         }
     }
 
     var weight: Font.Weight {
         switch self {
-        case .display, .titleL, .titleM, .heading, .label, .mono:
+        case .displayHero, .displayCompact, .display:
+            return .black
+        case .titleLg, .titleMd, .titleSm, .titleL, .titleM, .heading:
+            return .bold
+        case .scriptLg, .scriptXl, .scriptDisplay, .labelCaps, .monoButton, .monoTimestamp, .counter, .mono:
             return .semibold
-        case .bodyL, .bodyM:
-            return .regular
-        case .caption:
+        case .body, .bodyL, .bodyCompact, .bodyM:
+            return .medium
+        case .caption, .label:
             return .medium
         }
     }
 
     var design: Font.Design {
-        self == .mono ? .monospaced : .default
+        switch self {
+        case .labelCaps, .monoTimestamp, .monoButton, .counter, .mono:
+            return .monospaced
+        case .displayHero, .displayCompact, .display:
+            return .default
+        default:
+            return .default
+        }
+    }
+
+    var width: Font.Width? {
+        switch self {
+        case .displayHero, .displayCompact, .display:
+            return .condensed
+        default:
+            return nil
+        }
     }
 
     var font: Font {
         .system(size: size, weight: weight, design: design)
     }
+
+    var tracking: CGFloat {
+        switch self {
+        case .labelCaps, .monoButton:
+            return 1.6
+        case .monoTimestamp:
+            return 1.3
+        case .counter:
+            return 1.5
+        default:
+            return 0
+        }
+    }
 }
 
 enum FCSpacingToken: CGFloat, CaseIterable {
+    case s2 = 2
+    case s6 = 6
     case s4 = 4
     case s8 = 8
     case s12 = 12
@@ -135,13 +215,21 @@ enum FCSpacingToken: CGFloat, CaseIterable {
     case s24 = 24
     case s32 = 32
     case s40 = 40
+    case s48 = 48
+    case s64 = 64
 }
 
 enum FCShapeToken: CGFloat, CaseIterable {
+    case radius2 = 2
+    case radius4 = 4
     case radius10 = 10
+    case radius16 = 16
+    case radius20 = 20
     case radius14 = 14
     case radius18 = 18
     case radius24 = 24
+    case radius30 = 30
+    case radius40 = 40
     case capsule = 999
 }
 
@@ -188,9 +276,8 @@ enum FCMaterialToken: CaseIterable {
 
     var material: Material {
         switch self {
-        case .card: return .ultraThinMaterial
-        case .sheet: return .regularMaterial
-        case .overlay: return .thinMaterial
+        case .card, .sheet, .overlay:
+            return .thinMaterial
         }
     }
 }
@@ -275,6 +362,8 @@ extension View {
         let extraLeading = max(0, token.lineHeight - token.size)
         return self
             .font(token.font)
+            .fontWidth(token.width)
+            .tracking(token.tracking)
             .lineSpacing(extraLeading)
     }
 }
